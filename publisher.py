647057030865439ef86bb1de934e6369d8f14885
Luigi Sabetta <l.sabetta.g@gmail.com>
2023-10-02 12:30:15 +0200

    cleans up publisher

--- publisher.py
+++ publisher.py
@@ -1,17 +1,14 @@
 from __future__ import print_function
 import os.path
 import gspread
-#from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2.service_account import Credentials
-
-#from google.auth.transport.requests import Request
-#from google.oauth2.credentials import Credentials
-#from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from datetime import datetime
 from pygit2 import Repository
 import time
+from opts import OPT as opt
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
@@ -22,73 +19,82 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 SAMPLE_SPREADSHEET_ID = "1AdZdZJERaUironNl4vesg0e6k5Fk7BDEt-VNxXvgVSk"
 
 
-def publish(data, SAMPLE_RANGE_NAME):
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    json_key_file_path = '/home/luigi/Work/MachineUnlearning/publisher_key.json'
+import os
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
+def publish(data, range_name):
+    """Publishes data to a Google Sheet.
+
+    Args:
+        data (list): The data to be published.
+        range_name (str): The range in the Google Sheet where the data will be appended.
+
+    Returns:
+        None
+    """
+    credentials = None
+    json_key_file_path = opt.root_folder + 'publisher_key.json'
+
+    # Check if client_secrets.json file exists
     if os.path.exists('client_secrets.json'):
-        # Specify the path to your JSON key file
         credentials = Credentials.from_service_account_file(json_key_file_path, scopes=['https://www.googleapis.com/auth/spreadsheets'])
 
-        #credentials = Credentials.from_authorized_user_file(service_account_file, scopes=['https://www.googleapis.com/auth/spreadsheets'])
-
     try:
-        # Create a service client for Google Sheets
-
+        # Build the Google Sheets API service
         service = build('sheets', 'v4', credentials=credentials)
 
+        # Get the spreadsheets resource
         sheet = service.spreadsheets()
-        values = data # Modify this list with the data you want to write
 
-        # Find the last written row in the specified range
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
-       
+        # Prepare the values to be appended
+        values = data
+
+        # Get the last row in the range
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=range_name).execute()
         last_row = len(result.get('values', [])) + 1
 
-        # Append the data to the next empty row
-        range_to_append = SAMPLE_RANGE_NAME[:-4] + f'!A{last_row}:A{last_row}'
+        # Create the range to append the values
+        range_to_append = range_name[:-4] + f'!A{last_row}:A{last_row}'
+
+        # Create the request body
         body = {
             'values': values,
             'majorDimension': 'ROWS'
         }
+
+        # Create the append request
         request = sheet.values().append(
             spreadsheetId=SAMPLE_SPREADSHEET_ID,
             range=range_to_append,
             valueInputOption='USER_ENTERED',
             body=body
         )
+
+        # Execute the request
         response = request.execute()
+
+        # Print success message
         print('Data written successfully.')
 
     except HttpError as err:
         print(err)
 
-def push_results(args,  df_or_model=None, df_un_model=None, df_rt_model=None):
-    #keys = [str(k) for k in vars(args)]#+["test accuracy", "task accuracy"]
+def push_results(args, df_or_model=None, df_un_model=None, df_rt_model=None):
     blacklist = ["__module__", "__dict__", "__weakref__", "__doc__", "cuda","device", "n_classes", "classes_per_exp", "details", "num_workers", "root_folder", "verboseMLP", "data_path"]
+    
     repo = Repository(".")
     branch_name = repo.head.name.split("/")[-1]
     last_commit_id = repo[repo.head.target].hex
     date_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    
     params = [str(vars(args)[k]) for k in vars(args) if k not in blacklist] + [date_time, branch_name, last_commit_id]
-    SAMPLE_RANGE_NAME = 'Params!A:P'
-    max_retries = 3
-    retries = 0
-    while retries < max_retries:
-        try:
-            publish([params], SAMPLE_RANGE_NAME)
-            break
-        except:
-            print("Retrying...")
-            time.sleep(5)
-
-    SAMPLE_RANGE_NAME = 'Results!A:P'
+    range_name = 'Params!A:P'
+    
+    publish_with_retries([params], range_name)
+    
+    range_name = 'Results!A:P'
     results = [str(vars(args)["run_name"])]
     if df_or_model is not None:
         results.extend([df_or_model[i][0] for i in df_or_model])
@@ -103,21 +109,25 @@ def push_results(args,  df_or_model=None, df_un_model=None, df_rt_model=None):
     else:
         results.extend([None]*7)
     results = [str(r) for r in results]
-
-    #Code to prevent timeout error
+    
+    publish_with_retries([results], range_name)
+    
+#define publish with retries function to prevent timeot error
+def publish_with_retries(data, range_name):
     max_retries = 3
     retries = 0
     while retries < max_retries:
         try:
-            publish([results], SAMPLE_RANGE_NAME)
+            publish(data, range_name)
             break
         except:
             print("Retrying...")
             time.sleep(5)
             retries += 1
- 
-    #publish([keys])
+
+
 #define main beahvior
+
 if __name__ == "__main__":
     class ExampleClass:
         def __init__(self):
