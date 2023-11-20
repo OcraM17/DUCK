37a2a72d8aaeefb7b3786a9742d0070a2877e790
jbonato1 <bonato.jimi@gmail.com>
2023-11-20 14:53:36 +0100

    XMerge branch 'master' of github.com:OcraM17/MachineUnlearning

---
+++ training_oracle.py
@@ -0,0 +1,158 @@
+# Import necessary libraries
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from models.allcnn import AllCNN
+
+from opts import OPT as opt
+import os 
+
+from dsets import get_dsets_remove_class, get_dsets
+
+
+mean = {
+        'cifar10': (0.4914, 0.4822, 0.4465),
+        'cifar100': (0.5071, 0.4867, 0.4408),
+        'tinyImagenet': (0.485, 0.456, 0.406),
+        'VGG':(0.547, 0.460, 0.404)
+        }
+
+std = {
+        'cifar10': (0.2023, 0.1994, 0.2010),
+        'cifar100': (0.2675, 0.2565, 0.2761),
+        'tinyImagenet': (0.229, 0.224, 0.225),
+        'VGG':(0.323, 0.298, 0.263)
+        }
+
+
+
+transform_train = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean[opt.dataset], std=std[opt.dataset])
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean[opt.dataset], std=std[opt.dataset])
+])
+
+transform_train_tiny = transforms.Compose([
+        #transforms.ToPILImage(),
+        transforms.RandomCrop(64, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean[opt.dataset], std=std[opt.dataset])
+    ])
+
+transform_test_tiny = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean[opt.dataset], std=std[opt.dataset])
+    ])
+def trainer(class_to_remove, seed):
+    # Initialize the model
+    if opt.model == 'resnet18':
+        model= torchvision.models.resnet18(pretrained=True).to('cuda')
+    elif opt.model=='resnet34':
+        model= torchvision.models.resnet34(pretrained=True).to('cuda')
+    elif opt.model=='resnet50':
+        model = torchvision.models.resnet50(pretrained=True).to('cuda')
+    elif opt.model=='AllCNN':
+        model = AllCNN(n_channels=3, num_classes=opt.num_classes).to('cuda')
+
+    if opt.mode == 'HR':
+        if opt.dataset == "cifar10":
+            num=5000
+        elif opt.dataset == "cifar100":
+            num=5000
+        elif opt.dataset == "tinyImagenet":
+            num=10000
+
+        file_fgt = f'{opt.root_folder}forget_id_files/forget_idx_{num}_{opt.dataset}_seed_{seed}.txt'
+        _, test_loader, _, train_retain_loader = get_dsets(file_fgt=file_fgt)
+    if opt.mode == 'CR':
+        _, _, _, train_retain_loader, _, test_retain_loader = get_dsets_remove_class(class_to_remove)
+
+    if opt.dataset == 'cifar10':
+        os.mkdir('./chks_cifar10', exist_ok=True)
+    elif opt.dataset == 'cifar100':
+        os.mkdir('./chks_cifar100', exist_ok=True)
+        if 'resnet' in opt.model:    
+            model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False).to('cuda')
+            model.maxpool = nn.Identity()
+    elif opt.dataset == 'tinyImagenet':
+        #dataloader
+        if 'resnet' in opt.model:
+            model.fc = nn.Sequential(nn.Dropout(0.4), nn.Linear(model.fc.in_features, opt.num_classes)).to('cuda')
+
+    #dataloader
+    trainloader = torch.utils.data.DataLoader(train_retain_loader, batch_size=256, shuffle=True, num_workers=opt.num_workers)
+    if opt.mode == 'HR':
+        testloader = torch.utils.data.DataLoader(test_loader, batch_size=256, shuffle=False, num_workers=opt.num_workers)
+    elif opt.mode == 'CR':
+        testloader = torch.utils.data.DataLoader(test_retain_loader, batch_size=256, shuffle=False, num_workers=opt.num_workers)
+
+    epochs=300
+    criterion = nn.CrossEntropyLoss(label_smoothing=0.4)
+    optimizer = optim.SGD(model.parameters(), lr=0.1, weight_decay=5e-5)
+    if opt.dataset == 'tinyImagenet':
+        train_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.1) #learning rate decay
+    else:
+        train_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=300)
+
+
+    # Train the network
+    best_acc = 0.0
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        model.train()
+        correct, total = 0, 0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to('cuda'), labels.to('cuda')
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        train_acc = 100 * correct / total
+        train_loss = running_loss / len(trainloader)
+        train_scheduler.step()
+        if opt.mode == 'HR':
+            torch.save(model.state_dict(), f'chks_{opt.dataset}/best_checkpoint_without_{class_to_remove}.pth')
+        elif opt.mode == 'CR':
+            torch.save(model.state_dict(), f'chks_{opt.dataset}/chks_{opt.dataset}_seed_{seed}.pth')
+
+        if epoch % 5 == 0:        
+            model.eval()
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for data in testloader:
+                    inputs, labels = data
+                    inputs, labels = inputs.to('cuda'), labels.to('cuda')
+                    outputs = model(inputs)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+            val_acc = 100 * correct / total
+            print('Epoch: %d, Train Loss: %.3f, Train Acc: %.3f, Val Acc: %.3f, Best Acc: %.3f' % (epoch, train_loss, train_acc, val_acc, best_acc))
+    return best_acc
+
+
+if __name__ == '__main__':
+    for i in opt.seed:
+        if opt.mode == 'CR':
+            for class_to_remove in opt.class_to_remove:
+                best_acc = trainer(class_to_remove=class_to_remove,seed=i)
+        else:
+            best_acc = trainer(seed=i,class_to_remove=None)
---
+++ training_original.py
@@ -0,0 +1,141 @@
+# Import necessary libraries
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from torch.utils.data import Subset
+from models.allcnn import AllCNN
+from torch import nn
+
+from opts import OPT as opt
+import os 
+
+mean = {
+        'cifar10': (0.4914, 0.4822, 0.4465),
+        'cifar100': (0.5071, 0.4867, 0.4408),
+        'tinyImagenet': (0.485, 0.456, 0.406),
+        'VGG':(0.547, 0.460, 0.404)
+        }
+
+std = {
+        'cifar10': (0.2023, 0.1994, 0.2010),
+        'cifar100': (0.2675, 0.2565, 0.2761),
+        'tinyImagenet': (0.229, 0.224, 0.225),
+        'VGG':(0.323, 0.298, 0.263)
+        }
+
+
+
+transform_train = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean[opt.dataset], std=std[opt.dataset])
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean[opt.dataset], std=std[opt.dataset])
+])
+
+transform_train_tiny = transforms.Compose([
+        #transforms.ToPILImage(),
+        transforms.RandomCrop(64, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean[opt.dataset], std=std[opt.dataset])
+    ])
+
+transform_test_tiny = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean[opt.dataset], std=std[opt.dataset])
+    ])
+def trainer(removed=None):
+    # Initialize the model
+    if opt.model == 'resnet18':
+        model= torchvision.models.resnet18(pretrained=True).to('cuda')
+    elif opt.model=='resnet34':
+        model= torchvision.models.resnet34(pretrained=True).to('cuda')
+    elif opt.model=='resnet50':
+        model = torchvision.models.resnet50(pretrained=True).to('cuda')
+    elif opt.model=='AllCNN':
+        model = AllCNN(n_channels=3, num_classes=opt.num_classes).to('cuda')
+    
+    if opt.dataset == 'cifar10':
+        os.mkdir('./chks_cifar10', exist_ok=True)
+        # Load CIFAR-10 data
+        trainset = torchvision.datasets.CIFAR10(root=opt.data_path, train=True, download=True, transform=transform_train)
+        testset = torchvision.datasets.CIFAR10(root=opt.data_path, train=False, download=True, transform=transform_test)
+        
+    elif opt.dataset == 'cifar100':
+        os.mkdir('./chks_cifar100', exist_ok=True)
+        trainset = torchvision.datasets.CIFAR100(root=opt.data_path, train=True, download=True, transform=transform_train)
+        testset = torchvision.datasets.CIFAR100(root=opt.data_path, train=False, download=True, transform=transform_test)
+        if 'resnet' in opt.model:    
+            model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False).to('cuda')
+            model.maxpool = nn.Identity()
+    elif opt.dataset == 'tinyImagenet':
+        #dataloader
+        trainset = torchvision.datasets.ImageFolder(root=opt.data_path+'/tiny-imagenet-200/train',transform=transform_train_tiny)
+        testset = torchvision.datasets.ImageFolder(root=opt.data_path+'/tiny-imagenet-200/val/images',transform=transform_test_tiny)
+        if 'resnet' in opt.model:
+            model.fc = nn.Sequential(nn.Dropout(0.4), nn.Linear(model.fc.in_features, opt.num_classes)).to('cuda')
+
+    #dataloader
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True, num_workers=opt.num_workers)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=256, shuffle=False, num_workers=opt.num_workers)
+
+    epochs=300
+    criterion = nn.CrossEntropyLoss(label_smoothing=0.4)
+    optimizer = optim.SGD(model.parameters(), lr=0.1, weight_decay=5e-5)
+    if opt.dataset == 'tinyImagenet':
+        train_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.1) #learning rate decay
+    else:
+        train_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=300)
+
+
+    # Train the network
+    best_acc = 0.0
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        model.train()
+        correct, total = 0, 0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to('cuda'), labels.to('cuda')
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        train_acc = 100 * correct / total
+        train_loss = running_loss / len(trainloader)
+        train_scheduler.step()
+        torch.save(model.state_dict(), f'chks_{opt.dataset}/best_checkpoint_{opt.model}.pth')
+
+        if epoch % 5 == 0:        
+            model.eval()
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for data in testloader:
+                    inputs, labels = data
+                    inputs, labels = inputs.to('cuda'), labels.to('cuda')
+                    outputs = model(inputs)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+            val_acc = 100 * correct / total
+            print('Epoch: %d, Train Loss: %.3f, Train Acc: %.3f, Val Acc: %.3f, Best Acc: %.3f' % (epoch, train_loss, train_acc, val_acc, best_acc))
+    return best_acc
+
+
+if __name__ == '__main__':
+    trainer()
